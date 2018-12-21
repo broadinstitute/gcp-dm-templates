e17ec2c130a7f12853dbe514843b12503b891b59
Gregory Jordan <gjordan@verily.com>
2018-12-21 16:44:39 -0500

    Add a top-level firecloud-project template and schema.
    This template combines the project, network, and firewall templates into a template that can provision a complete FireCloud project, with Pub/Sub notifications at the start and end of provisioning.
    
    There are a few ugly parts, mostly to work around the issues that one cannot use a template-defined Deployment Manager node in a subsequent "dependsOn" stanza. Instead, we adopt a pattern where each template explicitly outputs a 'resourceNames' property, which can be referred to when a later node needs to be blocked by the template node.
    
    A future commit will add a README with example commands for (1) storing the firecloud-project template as a composite type and (2) instantiating a new firecloud-project deployment.

--- firecloud-project.py
+++ firecloud-project.py
@@ -33,9 +33,9 @@ FIRECLOUD_REQUIRED_APIS = [
     'storage-component.googleapis.com',
 ]
 
-FIREWALL_RESOURCE_NAME = 'firewall'
-PROJECT_RESOURCE_NAME = 'project'
-NETWORK_RESOURCE_NAME = 'network'
+FIREWALL_RESOURCE_NAME = 'fc-firewall'
+PROJECT_RESOURCE_NAME = 'fc-project'
+NETWORK_RESOURCE_NAME = 'fc-network'
 
 
 def create_network(context):
@@ -51,26 +51,30 @@ def create_network(context):
 
   return [{
       'type': 'templates/network.py',
-      'name': NETWORK_RESOURCE_NAME,
+      'name': 'fc-network',
       'properties': {
+          'name': 'network',
+          'projectId': '$(ref.fc-project.projectId)',
           'autoCreateSubnetworks': False,
           'subnetworks': subnetworks,
+          # We pass the dependsOn list into the network template as a
+          # parameter. Deployment Manager doesn't support dependsOn for
+          # template-call nodes, so we can't have this resource itself depend on
+          # the project-wide resources.
+          'dependsOn': '$(ref.fc-project.resourceNames)',
       },
-      'metadata': {
-          'dependsOn': [PROJECT_RESOURCE_NAME],
-      }
   }]
 
 
 def create_firewall(context):
   """Creates a VPC firewall config."""
-  return {
+  return [{
       'type': 'templates/firewall.py',
-      'name': FIREWALL_RESOURCE_NAME,
+      'name': 'fc-firewall',
       'properties': {
-          # This deployment manager reference string allows us to connect the
-          # firewall to the network, via the selfLink URL once it exists.
-          'network': '$(ref.{}.selfLink)'.format(NETWORK_RESOURCE_NAME),
+          'projectId': '$(ref.fc-project.projectId)',
+          'network': '$(ref.fc-network.selfLink)',
+          'dependsOn': '$(ref.fc-network.resourceNames)',
           'rules': [{
               'name': 'allow-icmp',
               'description': 'Allow ICMP from anywhere.',
@@ -107,14 +111,123 @@ def create_firewall(context):
               'targetTags': ['leonardo'],
           }],
       },
-      'metadata': {
-          'dependsOn': [NETWORK_RESOURCE_NAME],
-      }
-  }
+  }]
 
-def GenerateConfig(context):
+def create_iam_policies(context):
+  """Creates a list of IAM policies for the new project."""
+  organization_id = context.properties.get(
+      'parentOrganization', FIRECLOUD_ORGANIZATION_ID)
+  owners_and_viewers = [
+      'group:{}'.format(context.properties['projectOwnersGroup']),
+      'group:{}'.format(context.properties['projectViewersGroup']),
+  ]
+  owners_only = [
+      'group:{}'.format(context.properties['projectOwnersGroup']),
+  ]
+
+  # TODO: handle the RequesterPays role (which has slightly different names in
+  # different orgs) in an elegant way.
+  #
+  # Should look something like:
+  # {
+  #   # Owners & viewers are given the custom RequesterPays role to be able to
+  #   # pay for their own GCS bucket access.
+  #   'role': 'roles/{}/RequesterPays'.format(organization_id),
+  #   'members': owners_and_viewers,
+  # }
+
+  # TODO: there's an issue with granting cross-org roles/owner permission, so we
+  # can't currently assign roles/owner to the firecloud.org groups. This will be
+  # an issue for BYOO so we may have to adjust these permissions.
+  # {
+  #   # FireCloud requires project owner permission to handle project
+  #   # maintenance and ongoing updates to IAM configs.
+  #   'role': 'roles/owner',
+  #   'members': [
+  #      'user:billing@firecloud.org',
+  #      'group:firecloud-project-owners@firecloud.org'
+  #  ]
+  # }
+
+  return [{
+      # Rawls requires project editor permission to handle transactional IAM
+      # updates to the project.
+      #
+      # Cromwell requires project editor permission because _____ ?
+      'role': 'roles/editor',
+      'members': [
+          'serviceAccount:rawls-prod@broad-dsde-prod.iam.gserviceaccount.com',
+          'serviceAccount:cromwell-prod@broad-dsde-prod.iam.gserviceaccount.com',
+      ]
+  }, {
+      # Only FireCloud project owners are allowed to view the GCP project.
+      'role': 'roles/viewer',
+      'members': owners_only,
+  }, {
+      # Owners can manage billing on the GCP project (to switch out billing
+      # accounts).
+      'role': 'roles/billing.projectManager',
+      'members': owners_only,
+  }, {
+      # Owners & viewers are allowed to spin up PAPI nodes in the
+      # project (required for creating Leonardo notebooks).
+      'role': 'roles/genomics.pipelinesRunner',
+      'members': owners_and_viewers,
+  }, {
+      # Owners & viewers are allowed to run BigQuery queries in the project
+      # (required for running BQ queries within notebooks).
+      'role': 'roles/bigquery.jobUser',
+      'members': owners_and_viewers,
+  },
+  ]
+
+def create_pubsub_notification(context, depends_on, status_string):
+  """Creates a resource to publish a message upon deployment completion.
+
+  Arguments:
+    context: the DM context object.
+    depends_on: a list of resource names this notification should depend on.
+    status_string: the "status" attribute value to publish, e.g. 'STARTED' or
+      'COMPLETED'.
+
+  Returns:
+    A list of resource definitions.
+  """
+
+  return [{
+      'name': 'pubsub-notification-{}'.format(status_string),
+      'action': 'gcp-types/pubsub-v1:pubsub.projects.topics.publish',
+      'properties': {
+          'topic': context.properties['pubsubTopic'],
+          'messages': [{
+            'attributes': {
+                'projectId': context.properties['projectId'],
+                'status': status_string,
+            }
+          }]
+      },
+      'metadata': {
+          # The notification should only run after *all* project-related
+          # resources have been deployed.
+          'dependsOn': depends_on,
+          # Only trigger the pubsub message when the deployment is created (not on
+          # update or delete).
+          'runtimePolicy': ['UPDATE_ALWAYS'],
+      },
+  }]
+
+
+def generate_config(context):
   """Entry point, called by deployment manager."""
   resources = []
+
+  # Create an initial 'STARTED' pubsub notification.
+  if 'pubsubTopic' in context.properties:
+    resources.extend(create_pubsub_notification(
+        context,
+        depends_on=[],
+        status_string='STARTED',
+    ))
 
   # Required properties.
   billing_account_id = context.properties['billingAccountId']
@@ -122,24 +235,36 @@ def GenerateConfig(context):
 
   # Optional properties, with defaults.
   high_security_network = context.properties.get('highSecurityNetwork', False)
-  parent_obj = context.properties.get('parent', {
-      'id': FIRECLOUD_ORGANIZATION_ID,
-      'type': 'organization',
-  })
+
+  if 'parentFolder' in context.properties:
+    parent_obj = {
+        'id': context.properties['parentFolder'],
+        'type': 'folder'
+    }
+  elif 'parentOrganization' in context.properties:
+    parent_obj = {
+        'id': context.properties['parentOrganization'],
+        'type': 'organization'
+    }
+  else:
+    parent_obj = {
+        'id': FIRECLOUD_ORGANIZATION_ID,
+        'type': 'organization',
+    }
+
+  # Use a project name if given, otherwise it's safe to fallback to use the
+  # project ID as the name.
   project_name = context.properties.get('projectName', project_id)
   labels_obj = context.properties.get('labels', {})
 
   # Create the main project resource.
   resources.append({
       'type': 'templates/project.py',
-      'name': PROJECT_RESOURCE_NAME,
+      'name': 'fc-project',
       'properties': {
           'activateApis': FIRECLOUD_REQUIRED_APIS,
           'billingAccountId': billing_account_id,
-          # This causes APIs to be activated in parallel rather than serially. This
-          # dramatically speeds up the process, but may run into quota issues if too
-          # many projects are activated at the same time.
-          'concurrentApiActivation': False,
+          'iamPolicies': create_iam_policies(context),
           'labels': labels_obj,
           'name': project_name,
           # The project parent. For FireCloud, this should refer to the
@@ -160,7 +285,21 @@ def GenerateConfig(context):
   })
 
   if high_security_network:
-    resources.append(create_network(context))
-    resources.append(create_firewall(context))
+    resources.extend(create_network(context))
+    resources.extend(create_firewall(context))
+
+  if 'pubsubTopic' in context.properties:
+    resources.extend(create_pubsub_notification(
+        context,
+        # This is somewhat hacky, but we can't simply collect the name of each
+        # collected resource since template call nodes aren't "real" resources
+        # that can be part of a dependsOn stanza. So instead, we collect the
+        # names of all resources that are output by the network (which itself
+        # depends on the project). It doesn't seem to be possible to concatenate
+        # dependsOn arrays within the reference syntax, otherwise we could make
+        # this depend explicitly on all resources from the template nodes.
+        depends_on= '$(ref.fc-network.resourceNames)',
+        status_string='COMPLETED'
+    ))
 
   return {'resources': resources}
