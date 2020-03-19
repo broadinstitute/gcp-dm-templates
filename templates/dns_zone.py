5396b863b0b96af34d0c35b6e782897c61b120e4
Andrew Jang <andy7i@users.noreply.github.com>
2020-03-19 16:24:17 -0400

    wip - `create_dns_zone` and passing params

--- templates/dns_zone.py
+++ templates/dns_zone.py
@@ -2,22 +2,25 @@
 
 def generate_config(context):
   """ Entry point for the deployment resources. """
+  project = context.properties['projectId']
+  network_url = "https://www.googleapis.com/compute/v1/projects/{project}/global/networks/{network}".format(project=project,network=context.properties['networkName'])
 
   zone_resource = {
       'name': context.properties['resourceName'],
       # https://cloud.google.com/dns/docs/reference/v1/managedZones
+      # https://cloud.google.com/dns/docs/reference/v1/resourceRecordSets
       'type': 'gcp-types/dns-v1:managedZones',
       'properties': {
           'name': 'Private Google APIs access',
           'description': 'Routes googleapis.com to restricted.googleapis.com VIP',
           'dnsName': 'googleapis.com',
-          'project': context.properties['projectId'],
+          'project': project,
           'visibility': 'private',
           'privateVisibilityConfig': {
               'kind': 'dns#managedZonePrivateVisibilityConfigNetwork',
               'networks': [{
                   'kind': 'dns#managedZonePrivateVisibilityConfigNetwork',
-                  'networkUrl': context.properties['networkUrl']
+                  'networkUrl': network_url
               }]
           }
       }
