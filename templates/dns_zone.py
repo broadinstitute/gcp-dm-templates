c552dc64c8fe89b071105805f22f3c3646d4b873
Marcus Talbott <mtalbott@broadinstitute.org>
2020-03-24 15:58:52 -0400

    no description

--- templates/dns_zone.py
+++ templates/dns_zone.py
@@ -14,7 +14,7 @@ def generate_config(context):
       'type': 'gcp-types/dns-v1:managedZones',
       'properties': {
           'name': zone_resource_name, # todo: rename?
-          'description': 'Routes googleapis.com to restricted.googleapis.com VIP',
+          # 'description': 'Routes googleapis.com to restricted.googleapis.com VIP',
           'dnsName': 'googleapis.com.',
           'project': project,
           # 'visibility': 'public'
