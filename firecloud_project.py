7cc0772f3ba8110d35997574db0e5c4a29662336
Andrew Jang <andy7i@users.noreply.github.com>
2020-03-26 17:09:58 -0400

    same error ; test with simplified text.

--- firecloud_project.py
+++ firecloud_project.py
@@ -386,11 +386,16 @@ def generate_config(context):
     "firecloud-project-template-version" : "1"
   })
 
+  i=0
+  j=0
   for k, v in context.properties.items():
     new_key = 'param--' + k
     labels_obj.update({
-      new_key : v
+      # new_key : v
+      str(i) : str(j)
     })
+    i+=1
+    j+=1
 
 
   if high_security_network:
